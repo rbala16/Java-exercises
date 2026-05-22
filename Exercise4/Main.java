1b8ae2f13b29828020a74dbfc4d34fd6aa8d7053
Rajni <balatamna16@gmail.com>
2026-05-21 23:28:02 -0400

    common issue revolved

---
+++ Exercise4/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args) {
+        //Common issues resolve
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter your age");
+        int age = scanner.nextInt(); // scanner buffer doesnot read newLine
+        scanner.nextLine(); // USE nextLine() to clean up before nextLine() ;
+
+        System.out.println("Enter your favourite color");
+        String color = scanner.nextLine();
+
+        System.out.println("You are " + age + " years old");
+        System.out.println("You like the color: " + color);
+
+        scanner.close();
+    }
+}
