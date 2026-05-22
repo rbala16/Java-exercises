df3ecb6df0a551a77ba07247de1ebdae9a0fa89d
Rajni <balatamna16@gmail.com>
2026-05-21 23:44:50 -0400

    calculate area

---
+++ Exercise5/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String[] args){
+
+        //Calculate area of a rectangle
+
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the width:");
+        width = scanner.nextDouble();
+
+        System.out.println("Enter the height:");
+        height = scanner.nextDouble();
+
+        area = width * height;
+        System.out.println("The area is: " + area + " cm^2.");
+        scanner.close();
+    }
+}
