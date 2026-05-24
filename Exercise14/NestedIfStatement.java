7a2f77127c4470a50532fa183f17df1caee5b49c
Rajni <balatamna16@gmail.com>
2026-05-24 11:20:20 -0400

    nestedstatement implemented

---
+++ Exercise14/NestedIfStatement.java
@@ -0,0 +1,28 @@
+public class NestedIfStatement {
+    public static void main(String[] args) {
+
+        boolean isStudent = true;
+        boolean isSenior = true;
+        double price = 9.99;
+
+        if (isStudent) {
+            //NESTED IF STATEMENT
+            if (isSenior) {
+                System.out.println("You get a senior discount of %20");
+                System.out.println("You get a student discount of 10%");
+                price *= 0.7;
+            } else {
+                System.out.println("You get a student discount of 10%");
+                price *= 0.9;
+            }
+        }
+        else{
+            if(isSenior){
+                System.out.println("You get a senior discount of 20%");
+                price *= 0.8;
+            }
+        }
+
+        System.out.printf("The price of a ticket is : $%.2f" ,price);
+    }
+}
