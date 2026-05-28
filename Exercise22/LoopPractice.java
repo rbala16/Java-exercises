7b0d4bf0c00184b83df3535272fb323aeaa86b4b
Rajni <balatamna16@gmail.com>
2026-05-27 20:36:39 -0400

    for loop practice

---
+++ Exercise22/LoopPractice.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class LoopPractice {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter how many times you want to loop: ");
+        int max = scanner.nextInt();
+
+        for (int i = 1; i <= max; i++) {
+            System.out.println(i);
+        }
+        scanner.close();
+    }
+}
